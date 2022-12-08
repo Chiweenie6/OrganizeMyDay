11bf846c91a53428b70cb95c2483355a975d4700
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2022-12-07 18:10:40 -0700

    working on changeing color with time

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,54 @@ document.getElementById("currentTime").innerHTML = currentTime;
 
 
 
+// Changes the color on the time boxes depending on the current time.
+const timeBlock = document.getElementsByClassName("timeBlock");
+const timeHour = (new Date()).getHours();
+// $(".timeBlock").each(function() {
+//   var cTime = parseInt($(this).prop("id"));
+//   if(cTime < timeHour){
+//     $(this).css("background-color", "grey");
+//   } else if (cTime === timeHour) {
+//     $(this).css("background-color", "green");
+//   } else if (cTime > timeHour) {
+//     $(this).css("background-color", "lightblue");
+//   }
+// });
+ console.log(timeHour);
+console.log(parseInt(document.querySelectorAll("#id")));
+console.log(timeBlock.classList);
+
+
+
+
+// $(".timeBlock").each(function() {
+//   var cTime = parseInt($(this).prop("id"));
+//   if (cTime < timeHour){
+//     timeBlock.classList.add("past");
+
+//   }
+// });
+
+
+//  function changingColor() {
+//   var timeBlock = document.querySelectorAll(".timeBlock");
+//   timeBlock.classList.add("past");
+//  };
+
+//  changingColor();
+
+
+
+
+
+
+
+
+// function to store information locally using the save button
+$("button").click(function(){
+
+})
+
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -20,6 +68,15 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
+
+
+
+
+
+    
+
+
+
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
