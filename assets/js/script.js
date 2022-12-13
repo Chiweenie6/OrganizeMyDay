db1d95f844e6c8ba3349e059820d3c8c089e68c5
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2022-12-12 23:44:06 -0700

    almost there

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,6 @@ $(function() {
 var timeNow = dayjs().format("H");
 // console.log(timeNow);
 
-
 $(".timeBlock").each(function () {
   var timeBlockID = parseInt($(this).attr("id"));
 // console.log(timeBlockID);
@@ -24,45 +23,26 @@ $(".timeBlock").each(function () {
     $(this).addClass("future");
   }
 });
-
-
-
-
-
-
-
-// orderMyStuff();
-
-
-
-$(".storeInfoBtn").each(function () {
-  
-
-var orderMyStuff = JSON.parse(localStorage.getItem("myStuff"));
-
-  if (orderMyStuff !== null) {
-    document.querySelector(".activity").innerHTML = Object.values(orderMyStuff);
-  } 
-  
-
-  console.log(orderMyStuff);
 });
 
 
 
-// $ (function init() {
-//   letsOrderMyStuff();
 
-// })
+// Restoring the local storage data back into the planner
 
+$(".activity").each(function () {
+  
+var orderMyStuff = JSON.parse(localStorage.getItem("myStuff"));
 
-
-// gatherMyStuff.load(orderMyStuff);
-
-
-var gatherMyStuff = JSON.parse(localStorage.getItem("myStuff"));
-// console.log(gatherMyStuff);
-
+  if (orderMyStuff !== null) {
+    document.querySelector(".activity").textContent = Object.values(orderMyStuff);
+  } 
+  
+  
+  // console.log(Object.values(orderMyStuff));
+  // console.log(orderMyStuff);
+  // console.log($(this).siblings(".activity"));
+});
 
 
 
@@ -86,61 +66,3 @@ $(".storeInfoBtn").click(function() {
 
 
 });
-
-
-
-
-
-
-// $(".storeInfoBtn").click(function() {
-
-//   var savetron = $(".storeInfoBtn").index(this);
-
-//   console.log(savetron);
-
-//   myStuff[savetron] = $(".activity").text();
-
-//   console.log(myStuff);
-
-//   localStorage.setItem("myStuff", JSON.stringify(myStuff));
-// })
-
-// var myStuff = {};
-
-// var orderMyStuff = $(function() {
-//   var gatherMyStuff = JSON.parse(localStorage.getItem("savetron"));
-// });
-
-
-
-
-
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-
-
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });})
